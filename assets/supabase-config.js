4294e7bfc26faebe318e3ec1eacf2f3811f48062
sig511 <sig.511.115@gmail.com>
2026-08-07 07:17:25 +0900

    Add files via upload

---
+++ assets/supabase-config.js
@@ -0,0 +1,6 @@
+window.SIGNATURE_SUPABASE = {
+  url: "https://strohrujwxklgdntkrhb.supabase.co",
+  publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0cm9ocnVqd3hrbGdkbnRrcmhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5MDU3MzksImV4cCI6MjA5NDQ4MTczOX0._V6I0BinWtUw6KrntzZo5oITcO5S-eYS-m3TTQHsAyc",
+  functionUrl: "https://strohrujwxklgdntkrhb.supabase.co/functions/v1/board-api",
+  inquiryFunctionUrl: "https://strohrujwxklgdntkrhb.supabase.co/functions/v1/inquiry-api"
+};
